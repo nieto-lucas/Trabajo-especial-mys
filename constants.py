67a7a8308018cee977f934e8487504c5a56bb2bf
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2025-05-23 23:55:16 -0300

    perf: :zap: Agrego constantes para Generador Congruencial Lineal, Valor de dimensiones y tamaño de muestras

---
+++ constants.py
@@ -0,0 +1,21 @@
+"""
+    Generador Congruencial Lineal
+"""
+LCG_A = 16_807
+LCG_C = 0
+LCG_M = (2 ** 31) - 1
+
+
+"""
+    Valor de d: Distintas dimensiones para realizar las estimaciones
+""" 
+TWO_DIMENSIONS  = 2
+FIVE_DIMENSIONS = 5
+TEN_DIMENSIONS  = 10
+
+"""
+    Tamaños de muestras
+"""
+SAMPLE_SIZE_SMALL  = 10_000
+SAMPLE_SIZE_MEDIUM = 100_000
+SAMPLE_SIZE_BIG    = 1_000_000
