965c4d263ab035986a44977d663f52349a03769e
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2025-05-25 21:06:45 -0300

    build: :building_construction: Modifico MonteCarlo simplificado al problema

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -3,18 +3,17 @@ from rngs.RNG import RNG
 
 class MonteCarlo:
     """
-    Implementa el método de MonteCarlo multivariable
+    Implementa el método de MonteCarlo
     """
 
     @staticmethod
-    def method(Ndim: int, Nsamples: int,
+    def method(Nsamples: int,
                g: Callable[[float], float],
                rng: RNG) -> float:
         """
         Método de MonteCarlo para N variables
 
         Args:
-            Ndim (int): Número de dimensiones
             Nsamples (int): Número de muestras
             g (Callable[[float], float]): Función a aplicar
             random_method (rng: RNG): objeto de la clase RNG
@@ -24,6 +23,6 @@ class MonteCarlo:
         """
         integral = 0
         for _ in range(Nsamples):
-            U_list = [rng.rand01() for _ in range(Ndim)]
-            integral += g(*U_list)
+            U = rng.rand01()
+            integral += g(U)
         return integral / Nsamples
