8ea82cee7d371d9e2ce76d7ac8671e49742435f4
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2025-06-05 22:52:17 -0300

    feat: :sparkles: Agrego método para printear en 3D la distribucion de los puntos y método para printear nombre del método

--- rngs/RNG.py
+++ rngs/RNG.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from time import time
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class RNG(ABC):
     """
@@ -37,3 +39,30 @@ class RNG(ABC):
             float: siguiente número de la secuencia normalizado en [0, 1) 
         """
         pass
+
+    @abstractmethod
+    def name(self) -> str:
+        """
+        Devuelve el nombre del generador
+        """
+        pass
+
+    def plot_3d_distribution(self, Nsamples:int, color:str):
+        if Nsamples < 3:
+            raise ValueError("Se necesitan al menos 3 muestras.")
+        
+        values = [self.rand01() for _ in range(Nsamples)]
+
+        x_values = values[:-2]
+        y_values = values[1:-1]
+        z_values = values[2:]
+
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot(111, projection='3d')
+
+        ax.scatter(x_values, y_values, z_values, c=color,  marker='o', s=10, alpha=0.6)
+        ax.set_title(f"Distribución 3D de puntos de {self.name()}")
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+        plt.show()
